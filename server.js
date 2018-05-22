b7d2e4896e168a4de798e4d90cd6d17abcc007a8
unknown <mattcmej@gmail.com>
2018-05-21 20:00:23 -0700

    Add some initial files and begin working on server.js.

---
+++ server.js
@@ -0,0 +1,23 @@
+var bodyParser = require('body-parser')
+var express = require('express')
+var app = express()
+
+var PORT = process.env.PORT || 3000;
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+ 
+app.use(function (req, res) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.write('you posted:\n')
+    res.end(JSON.stringify(req.body, null, 2))
+})
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+})
