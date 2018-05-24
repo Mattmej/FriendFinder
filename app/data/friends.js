003afb609c9508cd4e4b7c4da1e3a749757a2e2a
unknown <mattcmej@gmail.com>
2018-05-23 20:09:22 -0700

    Create one entry in the friendArray in friends.js.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,8 +2,8 @@ var friendArray = [
     {
         name: "John",
         photo: "https://johnsite.john/john.jpg",
-        scores: []
-    }
+        scores: ["3", "2", "5", "3", "2", "1", "4", "3", "5", "2"]
+    },
 ];
 
 module.exports(friendArray);
