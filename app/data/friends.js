6d2f880f61bc81161e298e93f1176ee754a6f5a8
unknown <mattcmej@gmail.com>
2018-05-24 17:05:39 -0700

    Work on getting total array score in friends.js, and realize that I may need a constructor function.

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,15 +5,27 @@ var friendArray = [
     {
         name: "John",
         photo: "https://johnsite.john/john.jpg",
-        scores: ["3", "2", "5", "3", "2", "1", "4", "3", "5", "2"]
+        // scores: ["3", "2", "5", "3", "2", "1", "4", "3", "5", "2"]
+        scores: [3, 2, 5, 3, 2, 1, 4, 3, 5, 2],
+        // totalScore: friendArray[0].scores.reduce(arraySum)
+        totalScore: function() {
+            return this.scores.reduce(arraySum);
+        }
     },
 
     {
         name: "Stephen McStephenson",
         photo: "http://r.ddmcdn.com/s_f/o_1/APL/uploads/2014/10/small-pets5.jpg",
-        scores: ["5", "3", "2", "4", "3", "1", "5", "2", "4", "2"]
+        // scores: ["5", "3", "2", "4", "3", "1", "5", "2", "4", "2"]
+        scores: [5, 3, 2, 4, 3, 1, 5, 2, 4, 2],
+        // totalScore: friendArray[1].scores.reduce(arraySum)
+
     }
 ];
+
+function arraySum(total, num) {
+    return total + num;
+}
 
 // We can now use this array in any other file we want.
 module.exports = friendArray;
