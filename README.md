6ff0f1557afdbab6835d61804cc90008d190dd95
unknown <mattcmej@gmail.com>
2018-05-27 23:14:33 -0700

    Delete some extraneous comments in various files, clean up some incomplete fields, and finish readme file.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # FriendFinder
+
+### Description
+
+This project is an example of a full-stack application; front-end components are present, but these components both send and receive information from a server. For this project, my computer fulfilled the role of a server via Node.js. 
+
+This application takes the form of a social media site in which users can both meet with other member and become a member themselves. After answering a questionnaire, the user is matched up with a friend who has their info on the website's server, and then the user's information is stored in the server as well. 
+
+### Required Items
+
+##### Node Packages
+* Express
+* body-parser
+
+##### Other
+* Node.js
+* Nodemon (optional)
+
+
+### Details
+
+This application consists of multiple interconnecting files. Due to this modularity, each of the files will be explained below.
+
+##### server.js
+
+This file is the "center" of the application. The file is in charge of creating a server via Express.js, linking the files in the "routes" folder to the server, and listening for any changes to the server. 
+
+##### htmlRoutes.js & apiRoutes.js
+
+These two JavaScript files help determine which urls are accepted by the server and where these urls direct the user. The htmlRoutes file handles any routes linking to html pages, while the apiRoutes file handles any routes linking to API pages. 
+
+##### friends.js
+
+This file contains the initial values stored in the server. Also, this file ensures that the server's contents will be accessible to users via an API.
+
+##### home.html
+
+This html file is the homepage for the FriendFinder application. The page contains a link leading to the survey page and another link to the information in the API.
+
+##### survey.html
+
+This file contains a form which users can complete in order to be matched with a friend with similar interests. As of this moment, the user must complete all fields of the form, otherwise their information will not be stored correctly and the application may not function as intended. This functionality will be patched in a future release.
+
+
+
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,35 +1,5 @@
 
 
-
-// var friendArray = [
-//     {
-//         name: "John",
-//         photo: "https://johnsite.john/john.jpg",
-//         // scores: ["3", "2", "5", "3", "2", "1", "4", "3", "5", "2"]
-//         scores: [3, 2, 5, 3, 2, 1, 4, 3, 5, 2],
-//         // totalScore: friendArray[0].scores.reduce(arraySum)
-//         totalScore: function() {
-//             return this.scores.reduce(arraySum);
-//         }
-//     },
-
-//     {
-//         name: "Stephen McStephenson",
-//         photo: "http://r.ddmcdn.com/s_f/o_1/APL/uploads/2014/10/small-pets5.jpg",
-//         // scores: ["5", "3", "2", "4", "3", "1", "5", "2", "4", "2"]
-//         scores: [5, 3, 2, 4, 3, 1, 5, 2, 4, 2],
-//         // totalScore: friendArray[1].scores.reduce(arraySum)
-
-//     }
-// ];
-
-// function arraySum(total, num) {
-//     return total + num;
-// }
-
-// // We can now use this array in any other file we want.
-// module.exports = friendArray;
-
 ////////////////////////////////////////////////////////////////////////
 
 /*
@@ -49,33 +19,19 @@ var friendArray = [];
 function Friend (name, photo, scores) {
     this.name = name;
     this.photo = photo;
-    // this.scores = scores;
     this['scores[]'] = scores;
-    // this.getTotal = this.scores.reduce(total + num);
-    // this.totalScore = totalScore;
-    // this.getTotal = function(total, num) {
-    //     return total + num;
-    // }
-    // this.totalScore = this['scores[]'].reduce(this.getTotal);
+
 }
 
 var John = new Friend("John", "https://images.pexels.com/photos/683381/pexels-photo-683381.jpeg?auto=compress&cs=tinysrgb&h=350", [3, 2, 5, 3, 2, 1, 4, 3, 5, 2]);
 friendArray.push(John);
 
-// console.log(John.getTotal);
-// console.log(John.totalScore);
+
 
 var Stephen = new Friend("Stephen McStephenson", "http://r.ddmcdn.com/s_f/o_1/APL/uploads/2014/10/small-pets5.jpg", [5, 3, 2, 4, 3, 1, 5, 2, 4, 2]);
 friendArray.push(Stephen);
 
-// console.log(Stephen);
-// $(friendArray).push(John);
-// $(fruendArray).push(Stephen);
 
-// console.log(friendArray);
-
-
-// console.log(friendArray);
 
 var Bradley = new Friend("Bradley the Bro", "http://quimbys.com/blog/wp-content/uploads/2008/01/frat_boy.jpg", [1, 1, 5, 4, 5, 5, 1, 5, 2, 1]);
 friendArray.push(Bradley);
